f4548b0789308b5d50accc5f150bbf82245b94dd
chriscrea8 <christianchukwu84@gmail.com>
2022-08-13 18:57:24 +0100

    fetch user list from api and display on card-list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css'
 
 class App extends Component {
@@ -6,29 +7,24 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 1,
-        },
-        {
-          name: 'Dracula',
-          id: 2,
-        },
-        {
-          name: 'Zombie',
-          id: 3,
-        },
-      ],
+      monsters: [],
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
+        <CardList name={'Blessed'}>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> {monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     )
   }
